3e462080b1d906718742120a64c2de97bdd7552b
Yusuf <Yusuf@MATEBOOK>
2023-06-18 01:59:36 +0300

    Add project files.

---
+++ Test/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+
+// For kullanmadan 100 kere ekrana çıktı ver.
+
+int i = 0;
+while (i < 100)
+{
+
+
+    Console.WriteLine("For kullanmadım.");
+    i++;
+}
+
+
+
+
+
+
